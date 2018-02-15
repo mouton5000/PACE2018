d00bbc09f59567507148cf02dc9c8528dc40b574
Dimitri WATEL <dimitri.watel@ensiie.fr>
2018-02-15 19:55:53 +0100

    Finish flac

--- algos/flac.py
+++ algos/flac.py
@@ -12,6 +12,17 @@ def compute(instance):
     # The solution to be returned
     tree = set()
 
+    # Associate the couples of nodes (u,v) and (v,u) to the edge {u,v}
+    couple_to_edges = {}
+    for e in instance.g.edges:
+        u, v = e.extremities
+        couple_to_edges[(u, v)] = e
+        couple_to_edges[(v, u)] = e
+
+    def get_weight(u,  v):
+        """ Return the weight of the couple associated with the couple (u, v). """
+        return instance.weights[couple_to_edges[(u, v)]]
+
     # Copy of the required vertices of the instance. It let the algorithm
     # modify this set without modifiying the instance.
     required_vertices = set(instance.terms)
@@ -24,7 +35,19 @@ def compute(instance):
     reached = {root}
 
     # For each node, this dict sorts its input arcs by cost.
-    sorted_edges = {v: sorted(v.incident_edges, key=lambda e: instance.weights[e]) for v in instance.g}
+    sorted_input_arcs = defaultdict()
+    def get_sorted_input_arcs(v):
+        try:
+            return sorted_input_arcs[v]
+        except KeyError:
+            def edge_to_arc(e):
+                u, w = e.extremities
+                if v == u:
+                    return w, v
+                else:
+                    return u, v
+            arcs = sorted_input_arcs[v] = list(map(edge_to_arc, sorted(v.incident_edges, key=lambda e: instance.weights[e])))
+            return arcs
 
     #
     # "Local" parameters: those parameters are reinitialized at each iteration
@@ -52,22 +75,53 @@ def compute(instance):
     next_saturated_entering_arc_iterators = {}
 
     # For each node, this dict saves the next saturated entering arc of this node.
-    next_saturated_entering_arc = {}
+    next_saturated_entering_arcs = {}
 
     # The actual "physical" time since the beginning of saturation
     time = 0
 
     def update_next_saturated_arc(v):
-        pass
+
+        # print('Update', v, end=' ')
+
+        # Iterator over the entering arcs of v sorted by weights
+        try:
+            it = next_saturated_entering_arc_iterators[v]
+        except KeyError:
+            it = next_saturated_entering_arc_iterators[v] = iter(get_sorted_input_arcs(v))
+            # print(list(get_sorted_input_arcs(v)))
+
+        # Last saturated arc entering v, may be None if the saturation in v just started
+        b = next_saturated_entering_arcs.get(v, None)
+
+        try:
+            # If b was not the arc entering v with biggest cost, a exists
+            # print('b:', b, 'v:', v, end=' ')
+            next_saturated_entering_arcs[v] = a = next(it)
+            # print('a:', a)
+        except StopIteration:
+            # Else, all arcs entering v are already saturated
+            # print('No a available')
+            del next_saturated_entering_arcs[v]
+            del next_saturated_entering_arc_iterators[v]
+            return
+
+        # Compute saturated time of a
+        if b is None:
+            satTime = get_weight(*a) / len(sources[v])
+        else:
+            satTime = (get_weight(*a) - get_weight(*b)) / len(sources[v])
+        sorted_saturating[v] = (time + satTime, a[0] not in reached, v.index)
 
     def reinit():
-        """Clear the maps, sets and lists used by the algorithm FLAC. Reinitialize the parameters used by FLAC."""
+        nonlocal time
+        """ Clear the maps, sets and lists used by the algorithm FLAC. Reinitialize the parameters used by FLAC. """
         saturated.clear()
         sources.clear()
         sources_of_ancestors.clear()
         sorted_saturating.clear()
         next_saturated_entering_arc_iterators.clear()
-        next_saturated_entering_arc.clear()
+        next_saturated_entering_arcs.clear()
         time = 0
 
         for v in required_vertices:
@@ -79,16 +133,121 @@ def compute(instance):
             update_next_saturated_arc(v)
 
     def next_saturated_arc():
-        pass
+        """ Return the next saturated time entering the first node if the fibonnacci heap. """
+        nonlocal time
+        v, k = sorted_saturating.popitem()
+        time = k[0]
+        return next_saturated_entering_arcs[v]
 
-    def build_tree(u):
-        pass
+    def update_ancestor_sources(w, successors):
+        """
+        Update, for each node v which is a descendant of w in the list of successors, the set of sources
+        an ancestor of v can reach with a path of saturated arcs :
+        put all the sources an ancestor of w can reach into the vector of the successor v of w
+        and then recursively do it with v and all the descendant until the successor of v is null.
+        """
+        srcs = sources_of_ancestors[w]
+
+        toUpdate = successors.get(w, None)
+        while toUpdate is not None:
+            sources_of_ancestors[toUpdate] |= srcs
+            srcs = sources_of_ancestors[toUpdate]
+            toUpdate = successors.get(toUpdate, None)
 
     def find_conflict(u, v):
-        pass
+        """ Return true if the saturation of arc (u,v) implies a conflict. """
+        # Will contain the list of nodes linked to u with a saturated path including u
+        to_check = [u]
 
-    def saturate_arc_and_update(v):
-        pass
+        # If one of those nodes is already linked to one of the sources linked to v there is a conflict.
+        vsrcs = sources[v]
+
+        # Contain, for each node w, the successor of w in in path of saturated arc from w to u, if such a path exists.
+        successors = {}
+
+        # print('>', u, v)
+
+        while len(to_check) != 0:
+            w = to_check.pop(0)
+            # print('>>', w)
+            # If the sources reaching w intersect the sources reaching v there is a conflict
+            if not sources_of_ancestors[w].isdisjoint(vsrcs):
+                update_ancestor_sources(w, successors)
+                return True
+
+            saturated_input_arc = next_saturated_entering_arcs.get(w, None)
+            for u, _ in get_sorted_input_arcs(w):
+                if saturated_input_arc is not None and u == saturated_input_arc[0]:
+                    break
+                if (u, w) in saturated:
+                    to_check.append(u)
+                    successors[w] = u
+
+    def saturate_arc_and_update(u, v):
+        # This list will contain the set of node for which the flow rate will change after the saturation of a
+        to_update = [u]
+
+        vsrcs = sources[v]
+
+        # print('Start sat', u, v)
+
+        while len(to_update) != 0:
+            w = to_update.pop(0)
+            # print('Check', w)
+
+            # The current flow rate inside each entering arc of w, before a is saturated
+            previous_volume_flow_rate = len(sources[w])
+            sources[w] |= vsrcs  # disjoint union, because there is no conflict
+            sources_of_ancestors[w] |= vsrcs
+
+            if previous_volume_flow_rate != 0:
+                # if w already received flow before a became saturated
+                # the time the next entering arc of w is saturated is accelerated like this:
+                try:
+                    previous_sat_time, is_reached, index = sorted_saturating[w]
+                    new_volume_flow_rate = previous_volume_flow_rate + len(vsrcs)
+                    new_sat_time = time + (previous_sat_time - time) * previous_volume_flow_rate / new_volume_flow_rate
+                    sorted_saturating[w] = (new_sat_time, is_reached, index)
+                except KeyError:
+                    # All arcs entering w are fully saturated
+                    pass
+            else:
+                # if w did not receive any flow from the source, we initialize its saturation like this
+                update_next_saturated_arc(w)
+
+            # For each node linked to w with a saturated arc, we insert it in the list
+            # of nodes we have to update
+
+            saturating_input_arc = next_saturated_entering_arcs.get(w, None)
+            for input_arc in get_sorted_input_arcs(w):
+                if input_arc == saturating_input_arc:
+                    break
+                if input_arc in saturated:
+                    to_update.append(input_arc[0])
+
+        # print('Sat', (u, v))
+        saturated.add((u, v))
+
+    def build_tree(u):
+        to_check = [u]
+        tree = set()
+        reached = set()
+        leaves = set()
+
+        while len(to_check) != 0:
+            v = to_check.pop(0)
+            reached.add(v)
+            if v in required_vertices:
+                leaves.add(v)
+
+            for u, _ in get_sorted_input_arcs(v):
+                # We search for the "ouptut arcs" of v, as each "input arc" is an edge, if (u, v) is in the graph,
+                # (v, u) is too.
+                if not (v, u) in saturated:
+                    continue
+                tree.add(couple_to_edges[(v, u)])
+                to_check.append(u)
+        return tree, reached, leaves
 
     def apply_flac():
         """a tree rooted in the root of the instance spanning a part of the terminals, and the set of those
@@ -115,7 +274,7 @@ def compute(instance):
             # If there is a conflict, we just ignore the arc saturation, as if it was never added to the arc
             if not conflict:
                 # If there is no conflict, we have to update the flow rate of other arcs as a new arc is saturated.
-                saturate_arc_and_update(a)
+                saturate_arc_and_update(u, v)
 
     while len(required_vertices) != 0:
 
