5b41efe740c873f17e254ab9d6e6c19391ef636a
Dimitri Watel <watel.dimitri@gmail.com>
2018-02-25 18:06:47 +0100

    Correct SEED
    SEED is not given with the tests on optil.io

--- parameters.py
+++ parameters.py
@@ -1,7 +1,11 @@
-DEBUG = True
-
 import sys
-SEED = int(sys.argv[2])
-
 import random
+
+DEBUG = False
+
+if len(sys.argv) > 2:
+    SEED = int(sys.argv[2])
+else:
+    SEED = 4321
+
 random.seed(SEED)
